7eba6f8137496527f47df6f7b3cc8ba2d4fcab34
caleb dickson <calebdickson@calebs-MacBook-Pro.local>
2024-11-24 21:01:41 -0800

    bug fixes

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -2,7 +2,6 @@ import greekGateLoginPage from "../../assets/greekGateLoginPage.png";
 import orderlyIMG from "../../assets/orderlyIMG.jpeg";
 import portfolioMainPage from "../../assets/portfolioMainPage.png";
 import portfolio1 from "../../assets/portfolio1.png";
-import orderlySheet from "../../assets/orderlySheet.png";
 import greekGateChapterView from "../../assets/greekGateChapterView.png";
 import greekGateChapterDetails from "../../assets/greekGateChapterDetails.png";
 import greekGateFeed from "../../assets/greekGateFeed.png";
@@ -12,15 +11,16 @@ import port1cont from "../../assets/port1cont.png";
 import port1ed from "../../assets/port1ed.png";
 import port1proj from "../../assets/port1proj.png";
 import port1skills from "../../assets/port1skills.png";
+import orderlySpreadSheet from "../../assets/orderlySpreadSheet.png";
 
 export const projectsData = [
   {
     id: 1,
-    images: [orderlyIMG, orderlySheet], // Ensure images is an array
+    images: [orderlyIMG, orderlySpreadSheet], // Ensure images is an array
     title: "Orderly Site",
     category: "Web Development",
     description:
-      "The Orderly website was made to showcase the Orderly Software, whilst still ensuring a clean and professional look for the users. The website was made using HTML, React.JS, CSS, and JavaScript.",
+      "The Orderly website was made to showcase the Orderly Software, whilst still ensuring a clean and professional look for the users. The website was made using HTML, React.JS, CSS, and JavaScript. Currently in development for further use.",
   },
   {
     id: 2,
@@ -35,7 +35,7 @@ export const projectsData = [
     title: "Greek Gate",
     category: "Application Development",
     description:
-      "Greek Gate is a mobile application designed to streamline the proccess of managing fraternity and sorority recruitment events. The app was made using SwiftUI and XCode, ensuring user privacy, as well as a clean and professional look, in order to keep users coming back.",
+      "Greek Gate is a mobile application designed to streamline the proccess of managing fraternity and sorority recruitment events. The app was made using SwiftUI and XCode, ensuring user privacy, as well as a clean and professional look, in order to keep users coming back. Currently In Development.",
   },
   {
     id: 3,
@@ -55,11 +55,11 @@ export const projectsData = [
   },
   {
     id: 5,
-    images: [orderlySheet, orderlyIMG], // Ensure images is an array
+    images: [orderlySpreadSheet, orderlyIMG], // Ensure images is an array
     title: "Orderly Software",
     category: "Software Development",
     description:
-      "This software automates email tracking and provides real-time spreadsheet updates for efficient inventory management. Designed to parse order details directly from emails, it simplifies data entry, improves accuracy, and streamlines workflows for sellers, making tasks like inventory tracking and tax preparation seamless and hassle-free.",
+      "This software automates email tracking and provides real-time spreadsheet updates for efficient inventory management. Designed to parse order details directly from emails, it simplifies data entry, improves accuracy, and streamlines workflows for sellers, making tasks like inventory tracking and tax preparation seamless and hassle-free. Software leverages several API's to provide real-time updates and notifications. Currently still in development.",
   },
 ];
 
